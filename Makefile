d5236202142489b20b929d481afcf1fb1cc8b45f
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2023-04-20 16:22:43 +0200

    working tested miller

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
-
 all:
-	gcc main.c
+	#`gcc -fsanitize=unsigned-integer-overflow -Wall -Werror -Wextra main.c -lm
+	#https://stackoverflow.com/questions/23736507/is-there-a-reason-why-not-to-use-link-time-optimization-lto
+	gcc -O3 -flto -Wall -Werror -Wextra main.c -lm
--- main.c
+++ main.c
@@ -1,58 +1,60 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 //https://stackoverflow.com/questions/12168348/ways-to-do-modulo-multiplication-with-primitive-types
 //https://en.wikipedia.org/wiki/Modular_exponentiation
 
 
-uint64_t mul_modw(uint64_t a, uint64_t b, uint64_t m) {
-	//wiki formula, only up to 63 bits?
-	// so we want to calculate a * b (mod m)
-	long double x;
-	uint64_t c;
-	int64_t r;
-	//https://en.wikipedia.org/wiki/Modular_arithmetic#Properties
-	//it follows from the (compatibility with multiplication)
-	//rule that you can just mod a and b and get the same thing
-	//I think this is almost never relevant for our case, because usually
-	//m is enormous
-	if (a >= m) a %= m;
-	if (b >= m) b %= m;
-	//make more space for a
-	x = a;
-	c = x * b / m;
-	r = (int64_t) (a * b - c * m) % (int64_t_) m;
-	// r = (a * b - (x * b / m) * m) % m;
-	// r = (a * b - (a * b / m) * m) % m;
-	// r = (a * b - (a * b )) % m;
-	return r < 0 ? r + m : r;
+uint64_t mulmod(uint64_t a, uint64_t b, uint64_t m);
+uint64_t mod_pow(uint64_t b, uint64_t e, uint64_t m) {
+	//apparently from bruce schneier
+	//https://en.wikipedia.org/wiki/Modular_exponentiation
+	if (m == 1) return 0;
+	//assert?
+	uint64_t res = 1;
+	b %= m;
+	while (e > 0) {
+		if (e % 2 == 1) {
+			res = mulmod(res, b, m);
+		}
+		e >>= 1;
+		b = mulmod(b, b, m);
+	}
+	return res;
 }
 
+uint64_t mulmod(uint64_t a, uint64_t b, uint64_t m) {
+	//https://stackoverflow.com/a/18680280
+    uint64_t res = 0;
+    uint64_t temp_b;
 
-uint64_t apowdmodnw(uint64_t a, uint64_t d, uint64_t n) {
-	//wiki formula, only up to 63 bits?
-    uint64_t r = n == 1 ? 0 : 1;
-    while (d > 0) {
-        if (d & 1) r = mul_modw(r, a, n);
-        d = d >> 1;
-        a = mul_modw(a, a, n);
+    /* Only needed if b may be >= m */
+    if (b >= m) {
+        if (m > UINT64_MAX / 2u)
+            b -= m;
+        else
+            b %= m;
     }
-    return r;
-}
 
-uint64_t mod_pow(uint64_t a, uint64_t b, uint64_t n) {
-	//simple, but b can be enormous, so can be very slow
-	if (n == 0) {
-		fprintf(stderr, "mod 0 is impossible (in mod_pow)");
-	}
-	if (n == 1) return 0;
-	if (b == 0) return 1;
-	uint64_t result = 1;
-	for (uint64_t e == 1; e <= b; e++) {
-		result * = a;
-		result %= n;
-	}
-	return result;
+    while (a != 0) {
+        if (a & 1) {
+            /* Add b to res, modulo m, without overflow */
+            if (b >= m - res) /* Equiv to if (res + b >= m), without overflow */
+                res -= m;
+            res += b;
+        }
+        a >>= 1;
+
+        /* Double b, modulo m */
+        temp_b = b;
+        if (b >= m - b)       /* Equiv to if (2 * b >= m), without overflow */
+            temp_b -= m;
+        b += temp_b;
+    }
+    return res;
 }
 
 bool is_prime_miller(uint64_t n) {
@@ -64,21 +66,31 @@ bool is_prime_miller(uint64_t n) {
 	while ((n - 1) % (1 << (s + 1)) == 0) {
 		s++;
 	}
-	uint64_t d = (n - 1) / (1 << s);
-	uint64_t checks = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+	uint64_t d = (n - 1) / (1UL << s);
+	uint64_t checks[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
 	uint64_t len_checks = sizeof(checks) / sizeof(checks[0]);
 
+	//printf("n: %lu, s: %lu, d: %lu", n, s, d);
+	//printf(" 1<<s: %lu\n", 1UL << s);
 	for (uint64_t check_i = 0; check_i < len_checks; check_i++) {
 		uint64_t a = checks[check_i];
+		if (a >= n -2) {
+			break;
+		}
 		uint64_t x = mod_pow(a, d, n);
+		//printf("a: %lu, d: %lu, n: %lu, x: %lu\n", a, d, n, x);
+		uint64_t y = 0;
 		for (uint64_t i = 0; i < s; i++) {
-			uint64_t y = (x * x) % n;
-			if (y == 1 && x != 1 and x != n-1) {
+			y = mulmod(x, x, n);
+			//printf("x: %lu, x*x: %lu, y: %lu\n", x, x*x, y);
+			if (y == 1 && x != 1 && x != n-1) {
+				//printf("hier\n");
 				return false;
 			}
 			x = y;
 		}
 		if (y != 1) {
+			//printf("hier2\n");
 			return false;
 		}
 	}
@@ -88,6 +100,27 @@ bool is_prime_miller(uint64_t n) {
 int main() {
 	//largest 64-bit prime
 	//https://www.mersenneforum.org/showthread.php?t=3471
-	int prime = 18446744073709551557;
+	uint64_t prime = 18446744073709551557UL;
+	/* for (uint64_t i = 0; i < 100; i++) { */
+	/* 	printf("%lu is %d\n", i, is_prime_miller(i)); */
+	/* } */
+
 	printf("%d\n", is_prime_miller(prime));
+	char buffer[100];
+	int tests = 1000;
+	for (uint64_t i = 0xFFFFFFFFFFFFFFFFUL; i > 0xFFFFFFFFFFFFFFFF - tests; i--) {
+		sprintf(buffer, "a=$(factor %lu | wc -w); [ $a -eq 2 ]", i);
+		if (is_prime_miller(i)) {
+			if (system(buffer) != 0) {
+				printf("%lu should not be prime!", i);
+				exit(1);
+			}
+		} else {
+			if (system(buffer) == 0) {
+				printf("%lu should be prime!", i);
+				exit(1);
+			}
+		}
+	}
 }
+//printf("%d\n", is_prime_miller(prime));
